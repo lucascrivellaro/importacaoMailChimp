61b0ecce88d1a4e5d4492a2218feb1bb911ed3ba
lucas <lucas@debian>
2014-08-15 18:22:00 -0300

    asd

---
+++ integracaoMailChimp/index.php
@@ -0,0 +1,426 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pt-br" lang="pt-br"
+	dir="ltr">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta name="author" content="Super User" />
+<title>Contato</title>
+<link href="./favicon.ico" rel="shortcut icon"
+	type="image/vnd.microsoft.icon" />
+<link rel="stylesheet"
+	href="/components/com_rsform/assets/css/front.css?v=42" type="text/css" />
+<link rel="stylesheet"
+	href="/plugins/system/jcemediabox/css/jcemediabox.css?version=118"
+	type="text/css" />
+<link rel="stylesheet"
+	href="/plugins/system/jcemediabox/themes/standard/css/style.css?version=118"
+	type="text/css" />
+<link rel="stylesheet"
+	href="/components/com_rsform/assets/calendar/calendar.css"
+	type="text/css" />
+<link rel="stylesheet"
+	href="/components/com_rsform/assets/css/front.css" type="text/css" />
+<link rel="stylesheet" href="./assets/style.css" type="text/css" />
+<style type="text/css">
+
+/* Styles for DJ Image Slider with module id 89 */
+#djslider-loader89 {
+	margin: 0 auto;
+	position: relative;
+	height: 400px;
+	width: 960px;
+}
+
+#djslider89 {
+	margin: 0 auto;
+	position: relative;
+	height: 400px;
+	width: 960px;
+	display: none;
+}
+
+#slider-container89 {
+	position: absolute;
+	overflow: hidden;
+	left: 0;
+	top: 0;
+	height: 400px;
+	width: 960px;
+}
+
+#djslider89 ul#slider89 {
+	margin: 0 !important;
+	padding: 0 !important;
+	border: 0 !important;
+}
+
+#djslider89 ul#slider89 li {
+	list-style: none outside !important;
+	float: left;
+	margin: 0 !important;
+	border: 0 !important;
+	padding: 0 0px 0px 0 !important;
+	position: relative;
+	height: 400px;
+	width: 960px;
+	background: none;
+	overflow: hidden;
+}
+
+#slider89 li img {
+	width: 960px;
+	height: auto;
+	border: 0 !important;
+	margin: 0 !important;
+}
+
+#slider89 li a img, #slider89 li a:hover img {
+	border: 0 !important;
+}
+
+/* Slide description area */
+#slider89 .slide-desc {
+	position: absolute;
+	bottom: 160px;
+	left: 0px;
+	width: 600px;
+}
+
+#slider89 .slide-desc-in {
+	position: relative;
+}
+
+#slider89 .slide-desc-bg {
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+}
+
+#slider89 .slide-desc-text {
+	position: relative;
+}
+
+#slider89 .slide-desc-text h3 {
+	display: block !important;
+}
+
+/* Navigation buttons */
+#navigation89 {
+	position: relative;
+	top: 140px;
+	margin: 0 0px;
+	text-align: center !important;
+}
+
+#prev89 {
+	cursor: pointer;
+	display: block;
+	position: absolute;
+	left: 0;
+	top: -99999px;
+}
+
+#next89 {
+	cursor: pointer;
+	display: block;
+	position: absolute;
+	right: 0;
+	top: -99999px;
+}
+
+#play89, #pause89 {
+	cursor: pointer;
+	display: block;
+	position: absolute;
+	left: 47%;
+	top: -99999px;
+}
+
+#cust-navigation89 {
+	position: absolute;
+	top: 10px;
+	right: 10px;
+	z-index: 15;
+	display: none;
+}
+</style>
+
+<script src="/media/system/js/mootools-core.js" type="text/javascript"></script>
+<script src="/media/system/js/core.js" type="text/javascript"></script>
+<script src="/media/system/js/caption.js" type="text/javascript"></script>
+<script src="/components/com_rsform/assets/js/script.js?v=42"
+	type="text/javascript"></script>
+<script src="/plugins/system/jcemediabox/js/jcemediabox.js?version=118"
+	type="text/javascript"></script>
+<script src="/components/com_rsform/assets/js/script.js"
+	type="text/javascript"></script>
+<script src="/media/system/js/mootools-more.js" type="text/javascript"></script>
+<script src="./assets/slider.js" type="text/javascript"></script>
+<script type="text/javascript">
+window.addEvent('load', function() {
+				new JCaption('img.caption');
+			});
+JCEMediaBox.init({popup:{width:"",height:"",legacy:0,lightbox:0,shadowbox:0,resize:1,icons:1,overlay:1,overlayopacity:0.8,overlaycolor:"#000000",fadespeed:500,scalespeed:500,hideobjects:0,scrolling:"fixed",close:2,labels:{'close':'Fechar','next':'Próximo','previous':'Anterior','cancel':'Cancelar','numbers':'{$current} de {$total}'},cookie_expiry:""},tooltip:{className:"tooltip",opacity:0.8,speed:150,position:"br",offsets:{x: 16, y: 16}},base:"/",imgpath:"plugins/system/jcemediabox/img",theme:"standard",themecustom:"",themepath:"plugins/system/jcemediabox/themes"});
+(function($){ window.addEvent('domready',function(){var Slider89 = new DJImageSlider({id: '89', slider_type: 2, slide_size: 960, visible_slides: 1, show_buttons: 0, show_arrows: 0, preload: 800},{auto: 0, transition: Fx.Transitions.linear, duration: 600, delay: 3600})}); })(document.id);
+  </script>
+<style>
+#userForm {
+	width: 340px;
+}
+
+fieldset.formFieldset {
+	border: 0 none;
+	margin-bottom: 10px;
+}
+
+fieldset.formFieldset legend {
+	border-bottom: 1px dotted #A1C63A;
+	color: #A1C63A;
+	display: none;
+	font-family: maven_proregular, Arial, Helvetica, sans-serif;
+	font-size: 35px;
+	font-weight: bold;
+	margin-bottom: 15px;
+	padding: 0;
+	width: 100%;
+}
+
+div.formCaption {
+	display: block;
+	font-weight: bold;
+	margin: 0 10px 0 0;
+	text-align: right;
+}
+
+fieldset.formFieldset div.formCaption2 {
+	margin: 0 0 3px;
+}
+
+#userForm table td, #userForm table th, #userForm table {
+	padding: 5px;
+}
+
+.rsform-text-box, .rsform-input-box {
+	border: medium none;
+	margin: 0 0 5px;
+	padding: 5px;
+	width: 300px;
+}
+
+.rsform-input-box {
+	
+}
+
+.rsform-captcha-box {
+	border: 1px solid #333333;
+	margin: 5px 0;
+	padding: 5px;
+}
+
+.rsform-submit-button, .rsform-reset-button {
+	background: none repeat scroll 0 0 hsl(87, 100%, 25%);
+	border-radius: 5px 5px 5px 5px;
+	color: hsl(0, 0%, 100%);
+	cursor: pointer;
+	font-family: maven_proregular, Arial, Helvetica, sans-serif;
+	font-weight: normal;
+	margin-right: 10px;
+	padding: 10px 20px;
+	text-transform: uppercase;
+}
+
+.rsform-submit-button:hover, .rsform-reset-button:hover {
+	background: none repeat scroll 0 0 #333333;
+}
+</style>
+
+<!-- The following line loads the template CSS file located in the template folder. -->
+<link rel="stylesheet" href="./css/template.css" type="text/css" />
+<link rel="stylesheet" href="./css/hornav.css" type="text/css" />
+<link rel="stylesheet" href="./css/extras.css" type="text/css" />
+<link rel="stylesheet" href="./css/vertnav.css" type="text/css" />
+<script src="js/jquery-1.8.1.min.js" type="text/javascript" ></script>
+<script src="./js/peralta.js" type="text/javascript"></script>
+
+
+
+</head>
+<body id="contato">
+
+
+	<div id="header">
+		<div id="container">
+			<div id="headerlogo">
+				<h1>
+
+
+					<div class="custom">
+						<p>
+							<a href="index.php"><img src="images/logo.png" alt="Via Rápida"
+								title="Via Rápida" /> </a>
+						</p>
+					</div>
+
+				</h1>
+			</div>
+			<div id="menusearch">
+				<div id="ja-mainnav">
+
+					<ul class="menu">
+						<li id="item-101"><a
+							href="http://www.juiceintime.com/index.php?option=com_content&amp;view=article&amp;id=51&amp;Itemid=101&amp;lang=pt_br"><span>Juicers</span>
+						</a></li>
+						<li id="item-105"><a
+							href="http://www.juiceintime.com/index.php?option=com_content&amp;view=article&amp;id=2&amp;Itemid=105&amp;lang=pt_br"><span>Frutas</span>
+						</a></li>
+						<li id="item-106"><a
+							href="http://www.juiceintime.com/index.php?option=com_content&amp;view=article&amp;id=5&amp;Itemid=106&amp;lang=pt_br"><span>Nós</span>
+						</a></li>
+						<li id="item-165"><a
+							href="http://www.juiceintime.com/index.php?option=com_content&amp;view=article&amp;id=52&amp;Itemid=165&amp;lang=pt_br"><span>Ação</span>
+						</a></li>
+						<li id="item-182" class="current active"><a
+							href="http://www.juiceintime.com/index.php?option=com_content&amp;view=article&amp;id=4&amp;Itemid=182&amp;lang=pt_br"><span>Contato</span>
+						</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="frontbanner">
+		<div id="container">
+
+						<ul id="slider89">
+							<li><img src="./images/slide01.png" alt="Slider Home - 01" /> <!-- Slide description area: START -->
+								<div class="slide-desc">
+									<div class="slide-desc-in">
+										<div class="slide-desc-bg"></div>
+										<div class="slide-desc-text">
+
+											<div class="slide-text">
+												<h1>Fazer suco</h1>
+												<h2>nunca foi tão natural</h2>
+											</div>
+
+											<div style="clear: both"></div>
+										</div>
+									</div>
+								</div> 
+							</li>
+						</ul>
+
+		
+
+		</div>
+	</div>
+	</div>
+	<div id="wrapper">
+		<div id="wrapper2">
+			<div id="container">
+				<div id="content_m">
+					<div class="maincol_full">
+						<div class="cont">
+
+							<div class="item-pagecontato">
+
+								<h2>Fale Conosco</h2>
+
+								<?php include_once './formulario.php';?>
+								
+								<p>Juice in Time Technology</p>
+								<p>
+									<a href="mailto:contato@juiceintime.com"></a><a
+										href="mailto:contato@juiceintime.com">contato@juiceintime.com</a>
+								</p>
+								<p>fones: +55 11 3297-7123&nbsp; /&nbsp; +55 11 99717-5780</p>
+								<p>&nbsp;</p>
+
+							</div>
+
+						</div>
+						<div class="clr"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	</div>
+	</div>
+	</div>
+	<div id="footer">
+		<div id="footer-menu">
+			<div id="container">
+
+				<ul class="menu">
+					<li id="item-267"><a
+						href="http://www.juiceintime.com/index.php?lang=pt_br"><span>Home</span>
+					</a></li>
+					<li id="item-268"><a
+						href="http://www.juiceintime.com/index.php?option=com_content&amp;view=article&amp;id=51&amp;Itemid=101&amp;lang=pt_br"><span>Juicers</span>
+					</a></li>
+					<li id="item-269"><a
+						href="http://www.juiceintime.com/index.php?option=com_content&amp;view=article&amp;id=2&amp;Itemid=105&amp;lang=pt_br"><span>Frutas</span>
+					</a></li>
+					<li id="item-270"><a
+						href="http://www.juiceintime.com/index.php?option=com_content&amp;view=article&amp;id=5&amp;Itemid=106&amp;lang=pt_br"><span>Nós</span>
+					</a></li>
+					<li id="item-271"><a
+						href="http://www.juiceintime.com/index.php?option=com_content&amp;view=article&amp;id=52&amp;Itemid=165&amp;lang=pt_br"><span>Ação</span>
+					</a></li>
+					<li id="item-273"><a
+						href="http://www.juiceintime.com/index.php?option=com_content&amp;view=article&amp;id=4&amp;Itemid=182&amp;lang=pt_br"><span>Contato</span>
+					</a></li>
+					<li id="item-274"><a
+						href="http://www.youtube.com/watch?v=tUP8a5aZnS0&feature=youtu.be"
+						target="_blank"><span>Conheça nosso sistema</span> </a></li>
+					<li id="item-275"><a
+						href="http://www.juiceintime.com/index.php?option=com_content&amp;view=article&amp;id=3&amp;Itemid=275&amp;lang=pt_br"><span>Porque
+								nossas polpas e juicers são legais</span> </a></li>
+				</ul>
+			</div>
+		</div>
+		<div id="copyright">
+			<div id="container">
+
+
+				<div class="custom">
+					<table style="width: 100%;" border="0" cellpadding="0"
+						cellspacing="0">
+						<tbody>
+							<tr>
+								<td style="width: 90px; vertical-align: middle; padding: 0;">
+									<div class="fb-like" data-send="false"
+										data-layout="button_count" data-width="90"
+										data-show-faces="false">&nbsp;</div>
+								</td>
+								<td style="width: 685px; vertical-align: middle; padding: 0;">
+									<p>Copyright © 2013 Juice in Time. Todos os Direitos
+										Reservados.</p>
+								</td>
+								<td style="width: 105px; vertical-align: middle; padding: 0;">
+									<p style="text-align: right;">Desenvolvido por</p>
+								</td>
+								<td style="width: 110px; vertical-align: middle; padding: 0;">
+									<p>
+										<a href="http://www.linkwell.com.br" title="Linkwell.com.br"
+											target="_blank"><img src="images/linkwell-logo.png"
+											alt="linkwell-logo" style="margin-left: 2px;" height="32"
+											width="106" /> </a>
+									</p>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+
+			</div>
+		</div>
+	</div>
+	</div>
+</body>
+</html>
+<?php
